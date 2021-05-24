99d2f0363dd931d65b9eb5eede399c444f16005a
lahirusudesh <164189x@uom.lk>
2021-05-24 22:29:57 +0530

    changed sp dashboad

--- frontend/src/components/ServiceProvider/ServiceProviderDashboard/serviceProviderDashboard.js
+++ frontend/src/components/ServiceProvider/ServiceProviderDashboard/serviceProviderDashboard.js
@@ -15,7 +15,11 @@ export default function ServiceProviderDashboard() {
     <div
       style={{
         backgroundImage:
+<<<<<<< HEAD
           "url(" + require("../../../assets/img/addPosts.png") + ")",
+=======
+          "url(" + require("../../../assets/img/header.jpg") + ")",
+>>>>>>> 9f478cc... background image fixed
         backgroundRepeat: "repeat-x",
         minHeight: 1000,
       }}
@@ -27,11 +31,27 @@ export default function ServiceProviderDashboard() {
         <br />
         <br />
         <br />
+<<<<<<< HEAD
         <h2 className="welcome-msg" style ={{color: "black", backgroundColor:"rgba(255,255,255,0.9)"}}>Thank You Join With Us</h2> <br />
         <br />
         <br />
         <div className="row">
           <div className="col-lg-6 ml-auto mr-auto sm-6">
+=======
+        <h2 className="welcome-msg">Thank You Join With Us</h2> <br />
+        <br />
+        <br />
+        <div className="row mt-3">
+          <div className="col-lg-6 sm-6">
+            <button
+              className="btn btn-primary w-50 bold-font"
+              onClick={() => onCreateProfileClick()}
+            >
+              Create Your Profile
+            </button>
+          </div>
+          <div className="col-lg-6 sm-6">
+>>>>>>> 9f478cc... background image fixed
             <button
               className="btn btn-primary w-50 bold-font"
               onClick={() => onAddNewPostClick()}
